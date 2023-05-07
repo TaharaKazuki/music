d2ef2d7aec321ff532cbc8f405ca5c9c45fd1650
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-05-07 17:15:48 +0900

    update

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,10 +1,37 @@
 import { RequestHandler } from "express"
 import { CreateUser } from "@/types/user"
+import nodemailer from "nodemailer"
 import User from "@/models/user"
+import EmailVerificationToken from "@/models/emailVerificationToken"
+import { MAILTRAP_USER, MAILTRAP_PASS } from "@/utils/variables"
+import { generateToken } from "@/utils/helper"
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body
 
   const user = await User.create({ name, email, password })
+
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: MAILTRAP_USER,
+      pass: MAILTRAP_PASS,
+    },
+  })
+
+  const token = generateToken()
+
+  await EmailVerificationToken.create({
+    owner: user._id,
+    token,
+  })
+
+  transport.sendMail({
+    to: user.email,
+    from: "auth@myapp.com",
+    html: `<h1>Your verification token is ${token}</h1>`,
+  })
+
   res.status(201).json({ user })
 }
