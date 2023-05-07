eec88415e2a4f47f8ca25023ff60e4da621e6c02
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-05-07 20:37:24 +0900

    update

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,10 +1,12 @@
 import { RequestHandler } from "express"
 import { CreateUser } from "@/types/user"
 import nodemailer from "nodemailer"
+import path from "path"
 import User from "@/models/user"
 import EmailVerificationToken from "@/models/emailVerificationToken"
 import { MAILTRAP_USER, MAILTRAP_PASS } from "@/utils/variables"
 import { generateToken } from "@/utils/helper"
+import { generateTemplate } from "@/mail/template"
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body
@@ -27,10 +29,32 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     token,
   })
 
+  const welcomeMessage = `Hi ${name}, welcome to Music! There are so much 
+  thing that we do for verified users. Use the given OTP to verify your email.`
+
   transport.sendMail({
     to: user.email,
     from: "auth@myapp.com",
-    html: `<h1>Your verification token is ${token}</h1>`,
+    html: generateTemplate({
+      title: "Welcome to Music",
+      message: welcomeMessage,
+      logo: "cid:logo",
+      banner: "cid:welcome",
+      link: "#",
+      btnTitle: token,
+    }),
+    attachments: [
+      {
+        filename: "logo.png",
+        path: path.join(__dirname, "../mail/logo.png"),
+        cid: "logo",
+      },
+      {
+        filename: "welcome.png",
+        path: path.join(__dirname, "../mail/welcome.png"),
+        cid: "welcome",
+      },
+    ],
   })
 
   res.status(201).json({ user })
--- server/src/models/emailVerificationToken.ts
+++ server/src/models/emailVerificationToken.ts
@@ -1,4 +1,5 @@
 import { ObjectId, Schema, model, Model } from "mongoose"
+import { hash, compare } from "bcrypt"
 
 type EmailVerificationTokenDocument = {
   owner: ObjectId
@@ -6,26 +7,45 @@ type EmailVerificationTokenDocument = {
   createdAt: Date
 }
 
-const emailVerificationTokenSchema = new Schema<EmailVerificationTokenDocument>(
-  {
-    owner: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-    token: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      expires: 60 * 60,
-      default: Date.now(),
-    },
+type Methods = {
+  compareToken(token: string): Promise<Boolean>
+}
+
+const emailVerificationTokenSchema = new Schema<
+  EmailVerificationTokenDocument,
+  {},
+  Methods
+>({
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    expires: 60 * 60,
+    default: Date.now(),
+  },
+})
+
+emailVerificationTokenSchema.pre("save", async function (next) {
+  if (this.isModified("token")) {
+    this.token = await hash(this.token, 10)
   }
-)
+
+  next()
+})
+
+emailVerificationTokenSchema.methods.compareToken = async function (token) {
+  const result = await compare(token, this.token)
+  return result
+}
 
 export default model(
   "EmailVerificationToken",
   emailVerificationTokenSchema
-) as Model<EmailVerificationTokenDocument>
+) as Model<EmailVerificationTokenDocument, {}, Methods>
